b37e99f90883a2606d50e94e171d4117839c06be
andy.bin <andy.bin@wolaidai.com>
2018-11-22 10:30:19 +0800

    init xcode project

---
+++ FaceRecognizer/FaceRecognizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceRecognizer
+//
+//  Created by andy.bin on 2018/11/22.
+//  Copyright © 2018 Binea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
